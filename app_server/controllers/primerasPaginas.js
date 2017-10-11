f3d0c81c51a257ddda78f500a710546994ec973e
Juanma Romero <xjuanma.romerox@gmail.com>
2017-10-11 11:53:07 -0300

    tercera version

--- app_server/controllers/primerasPaginas.js
+++ app_server/controllers/primerasPaginas.js
@@ -5,14 +5,14 @@ module.exports.home = function (req,res) {
     res.render('home', {title: 'Buscar servicio'});
 }
 module.exports.busqueda = function (req,res) {
-    res.render('index', {title: 'Listado de servicios'});
+    res.render('busqueda', {title: 'Listado de servicios'});
 }
 module.exports.servicio = function (req,res) {
-    res.render('index', {title: 'Servicio disponible'});
+    res.render('servicio', {title: 'Servicio disponible'});
 }
 module.exports.empresa = function (req,res) {
-    res.render('index', {title: 'Empresa'});
+    res.render('empresa', {title: 'Empresa'});
 }
 module.exports.sobreCombi = function (req,res) {
-    res.render('index', {title: 'Sobre CombisOnLine'});
+    res.render('sobreCOL', {title: 'Sobre CombisOnLine'});
 }
